290b4eab91110a752e80f2c158071406543ac4cd
yatsky <wyatsky@gmail.com>
2016-08-17 15:37:01 +1000

    add folders for records and pools

--- ReadFiles.py
+++ ReadFiles.py
@@ -14,9 +14,12 @@ def readfile(FileName):
     if FileName == '':
         FileName = 'MediTermsComplete'
         cfg.FileName = FileName
-
+		
+	directory = os.path.join(os.getcwd(), 'TermPools')
+	directory = os.path.join(directory, FileName)
+	
     try:
-        with open(FileName, 'r') as f:
+        with open(directory, 'r') as f:
             raw = f.readlines()
     except IOError:
         print 'File name terribly wrong!'
--- ReadStudyRecords.py
+++ ReadStudyRecords.py
@@ -4,14 +4,18 @@ import os
 
 def readstudyrecords(FileName, RawTerm):
     cw = os.getcwd()
-    srPath = cw + '\\' + FileName + 'StudyRecord'
+    directory = os.path.join(os.getcwd(), 'StudyRecords')
+    
+    srPath = os.path.join(directory, FileName + 'StudyRecord')
     # print srPath
     if not os.path.isfile(srPath):
-        with open(FileName + 'StudyRecord', 'w') as f:
+        # with open(FileName + 'StudyRecord', 'w') as f:
+        with open(srPath, 'w') as f:
             records = ['0'] * len(RawTerm)
             for record in records:
                 f.write('%s\n' % record)
                 
-    with open(FileName + 'StudyRecord', 'r') as f:
+    # with open(FileName + 'StudyRecord', 'r') as f:
+    with open(srPath, 'r') as f:
         records = f.readlines()
     return records
